804d0ec0f22a59161e74c07bedfd9eeaee5fb814
dsalazar31 <alejandrodanielsalazar@gmail.com>
2022-04-23 16:06:42 -0600

    diego

---
+++ parcial3/lib/datos.dart
@@ -0,0 +1,11 @@
+class Resultado {
+  final int albumId;
+  final int id;
+  final String titulo;
+  final String img1;
+  final String img2;
+
+  Resultado(this.albumId, this.id, this.titulo, this.img1, this.img2);
+}
+
+List<Resultado> resultado = [];
--- parcial3/lib/main.dart
+++ parcial3/lib/main.dart
@@ -1,115 +1,150 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:parcial3/datos.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Inicio());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Inicio extends StatelessWidget {
+  const Inicio({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Parcial 3",
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeState extends State<Home> {
+  Future<List<Resultado>> _getListado() async {
+    var url = Uri.parse("https://jsonplaceholder.typicode.com/photos");
+    final respuesta = await http.get(url);
+    if (respuesta.statusCode == 200) {
+      String body = utf8.decode(respuesta.bodyBytes);
+      final jsonDatos = jsonDecode(body);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+      for (var item in jsonDatos) {
+        resultado.add(Resultado(item["albumId"], item["id"], item["title"],
+            item["url"], item["thumbnailUrl"]));
+      }
+      return resultado;
+    } else {
+      return throw Exception("Error de conexión");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        appBar: AppBar(
+          title: const Text("Parcial 3"),
+        ),
+        body: detalle());
+  }
+
+  Widget titulo() {
+    return Container(
+      decoration: const BoxDecoration(
+          border: Border(bottom: BorderSide(color: Colors.black))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          texto("ID"),
+          texto("Titulo"),
+          texto("Imagen 1"),
+          texto("Imagen 2")
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+
+  Widget texto(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget detalle() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Container(
+        height: 500,
+        child: ListView(
+          children: [titulo(), listado()],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget listado() {
+    return FutureBuilder(
+      future: _getListado(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return SizedBox(
+            height: 600,
+            child: ListView(
+              children: _mostrarDatos(snapshot.data),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text("Actualize nuevamente"),
+          );
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
+
+  List<Widget> _mostrarDatos(data) {
+    int i = 0;
+    List<Widget> datosResultado = [];
+
+    for (var item in data) {
+      datosResultado.add(Container(
+        decoration: const BoxDecoration(
+            border: Border(
+                bottom: BorderSide(color: Color.fromARGB(255, 167, 166, 166)))),
+        height: 100,
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Text(resultado[i].id.toString()),
+          SizedBox(width: 75, child: Text(resultado[i].titulo)),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                image: DecorationImage(
+                    image: NetworkImage(resultado[i].img1), fit: BoxFit.cover)),
+            width: 70,
+            height: 70,
+          ),
+          SizedBox(
+              width: 50,
+              height: 50,
+              child: Image(image: NetworkImage(resultado[i].img2)))
+        ]),
+      ));
+      i++;
+    }
+    return datosResultado;
   }
 }
